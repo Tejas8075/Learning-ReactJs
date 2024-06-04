7796ab42015c6fcf063118de7aa06f223696bf03
Tejas8075 <mahangadetejas09@gmail.com>
2024-06-04 13:55:16 +0530

    Add new changes

--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,9 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+const anotherElement = (
+  <a href="https://google.com" target='_blank'>Visit google</a>
+)
+
+const anotherEelement = "Tejas";
+
+const reactElement = React.createElement(
+  'a' ,//Tag name
+  {href: 'https://google.com', target: '_blank'}, //set the attributes of the tag
+  'Cick me to visit google', //Text in the tag
+  anotherEelement //Evaluated Expressions = JS in react
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // anotherElement
+  reactElement
+  // Converts it to object as in custom react app
 )
