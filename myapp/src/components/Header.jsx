bebad50f2276e3a1e15b9c461bc6c9ce30f5061f
Tejas8075 <mahangadetejas09@gmail.com>
2024-06-14 13:56:50 +0530

    Add Raect basic syntax

---
+++ myapp/src/components/Header.jsx
@@ -0,0 +1,11 @@
+const Hey = () => {
+    return (
+        <nav>
+            <a href="home">Home</a>
+            <a href="about">About</a>
+            <a href="contact">Contact</a>
+        </nav>
+    )
+}
+
+export default Hey;
