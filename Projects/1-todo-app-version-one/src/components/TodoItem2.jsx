a9c747ec04a9b9a3943f92c488536a7d4352fa12
Tejas8075 <tejasmahangade61@gmail.com>
2025-03-12 22:08:27 +0530

    Project version 1 complete

---
+++ Projects/1-todo-app-version-one/src/components/TodoItem2.jsx
@@ -0,0 +1,21 @@
+function TodoItem2() {
+
+    return (
+        <div class="container">
+            <div class="row kg-row">
+                <div class="col-6">
+                    Go to College
+                </div>
+                <div class="col-4">
+                    04/10/2023
+                </div>
+                <div class="col-2">
+                    <button type="button" class="btn btn-danger kg-button">Delete</button>
+                </div>
+            </div>
+        </div>
+    )
+
+}
+
+export default TodoItem2;
